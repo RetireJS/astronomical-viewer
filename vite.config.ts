766c3e174147cae1887cafb87f126e37cce02227
Erlend Oftedal <erlend.oftedal@onersac.com>
2026-01-17 16:03:51 +0100

    Adds build script

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/astronomical-viewer/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
